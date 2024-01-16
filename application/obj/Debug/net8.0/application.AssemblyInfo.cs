3df702af85d4e0b6cb022f8b40d025d65acf61a5
Beanie98 <mirandapalm74@gmail.com>
2024-01-16 14:44:32 +0100

    test igen

--- application/obj/Debug/net8.0/application.AssemblyInfo.cs
+++ application/obj/Debug/net8.0/application.AssemblyInfo.cs
@@ -12,7 +12,6 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2eee95dd2c15cc7fdd989e9a3ec8ee3071d96687")]
 [assembly: System.Reflection.AssemblyProductAttribute("application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("application")]
